99b7224ed9a2f9fbd4fecbb2328db1eb822f1be3
antonioreisbefore <antonio.reis@before.com.br>
2023-05-17 09:15:04 -0400

    doc: updating readme.md feat: add bash to install sail in a computer without php and composer

---
+++ script/install-sail.sh
@@ -0,0 +1,9 @@
+echo "Installing composer dependencies in Docker Container"
+
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v $(pwd):/var/www/html \
+    -w /var/www/html \
+    laravelsail/php82-composer:latest \
+    composer install --ignore-platform-reqs
+
